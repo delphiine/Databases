8eac0d0edbf32d8cd629747a7932756a640b9f57
delphine <delphine.mubiligi@gmail.com>
2022-07-05 20:41:54 +0100

    removed unused info

--- designing_repository_class/book_store/app.rb
+++ designing_repository_class/book_store/app.rb
@@ -1,18 +1,7 @@
 require_relative 'lib/database_connection'
 
 # We need to give the database name to the method `connect`.
-Music Library:
-DatabaseConnection.connect('music_library')
-
-# Perform a SQL query on the database and get the result set.
-sql = 'SELECT id, title FROM albums;'
-result = DatabaseConnection.exec_params(sql, [])
-
-# Print out each record from the result set .
-result.each do |record|§
-
-Books:
-DatabaseConnection.connect('books')
+DatabaseConnection.connect('book_store_test')
 
 # Perform a SQL query on the database and get the result set.
 sql = 'SELECT id, title, author_name FROM books;'
