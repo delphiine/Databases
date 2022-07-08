8f37a9691e3a1e311846ca172513ea6f8b3119c6
delphine <delphine.mubiligi@gmail.com>
2022-07-08 11:56:00 +0100

    Wrapping the program in an Application class

--- designing_repository_class/music_library/app.rb
+++ designing_repository_class/music_library/app.rb
@@ -2,22 +2,55 @@ require_relative 'lib/database_connection'
 require_relative 'lib/artist_repository'
 require_relative 'lib/album_repository'
 
-# We need to give the database name to the method `connect`.
-DatabaseConnection.connect('music_library_test')
+class Application
+  # The Application class initializer
+  # takes four arguments:
+  #  * The database name to call `DatabaseConnection.connect`
+  #  * the Kernel object as `io` (so we can mock the IO in our tests)
+  #  * the AlbumRepository object (or a double of it)
+  #  * the ArtistRepository object (or a double of it)
+  def initialize(music_library, io, album_repository, artist_repository)
+    DatabaseConnection.connect(music_library)
+    @io = io
+    @album_repository = album_repository
+    @artist_repository = artist_repository
+  end
 
-# Perform a SQL query on the database and get the result set.
-album_repository = AlbumRepository.new
-artist_repository = ArtistRepository.new
+  def run
+    @io.puts "Welcome to the music library manager!"
+    @io.puts "What would you like to do?  
+    1 - List all albums   
+    2 - List all artists"
 
-p album = album_repository.find(2)
-p artist = artist_repository.find(2)
+    answer = @io.gets.chomp
+    num = 1
+    if answer == "1" 
+        @album_repository.all.each do |record|
+            puts "* #{num} - #{record.title}"
+            num += 1
+        end
+    else 
+        @artist_repository.all.each do |item|
+            puts "* #{num} - #{item.name}"
+            num += 1 
+        end
+    end
 
-# Print out each record from the result set .
+    # "Runs" the terminal application
+    # so it can ask the user to enter some input
+    # and then decide to run the appropriate action
+    # or behaviour.
+  end
+end
 
-# album_repository.each do |album|
-#   p album.title
-# end
-
-# artist_repository.each do |artist|
-#   p artist
-# end
+# If we run this file using `ruby app.rb`,
+# run the app.
+if __FILE__ == $0
+  app = Application.new(
+    'music_library',
+    Kernel,
+    AlbumRepository.new,
+    ArtistRepository.new
+  )
+  app.run
+end
